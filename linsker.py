0b66d18766fa3fbab1c1f916e1b8f2b879c23422
GonzaloJCC <gonzalojimenezcarretero@gmail.com>
2025-12-21 20:18:34 +0100

    Improvements

--- linsker.py
+++ linsker.py
@@ -18,16 +18,16 @@ df_plot = df.iloc[::50, :].copy()
 fig, (ax_i, ax_w) = plt.subplots(2, 1, figsize=(12, 8), sharex=True)
 
 # --- PANEL 1: Corrientes (i1, i2) ---
-ax_i.plot(df_plot['Time'], df_plot['i1'], label='i1', color='red', lw=0.7)
-ax_i.plot(df_plot['Time'], df_plot['i2'], label='i2', color='purple', lw=0.7)
+ax_i.plot(df_plot['Time'], df_plot['i1'], label='i1', color='red', lw=2.5)
+ax_i.plot(df_plot['Time'], df_plot['i2'], label='i2', color='purple', lw=2.5)
 ax_i.set_ylabel('Corriente (i)')
 ax_i.set_title('Evolución de Corrientes y Pesos')
 ax_i.legend(loc='upper right')
 ax_i.grid(True, alpha=0.3)
 
 # --- PANEL 2: Pesos (w1, w2) ---
-ax_w.plot(df_plot['Time'], df_plot['w1'], label='w1', color='brown', lw=1)
-ax_w.plot(df_plot['Time'], df_plot['w2'], label='w2', color='darkgreen', lw=1)
+ax_w.plot(df_plot['Time'], df_plot['w1'], label='w1', color='brown', lw=3.5)
+ax_w.plot(df_plot['Time'], df_plot['w2'], label='w2', color='darkgreen', lw=3.5)
 ax_w.set_ylabel('Pesos (w)')
 ax_w.set_xlabel('Tiempo (s)')
 ax_w.legend(loc='upper right')
